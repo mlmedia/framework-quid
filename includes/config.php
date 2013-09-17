bfe663ace9e0cb332e28f74d3b4f9cea2ede48c3
mlmedia <mike@dockstreetmedia.com>
2013-09-16 23:06:02 -0400

    - changed config

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,6 @@
 <?php
 
 // define config constants for site-wide includes with different directory structure
-define("BASEURL", "http://localhost/quid");
-define("BASEPATH", "C:/xampp/htdocs/quid");
+define("BASEURL", "http://localhost/__DEV/quid");
+define("BASEPATH", "C:/xampp/htdocs/__DEV/quid");
 ?>
