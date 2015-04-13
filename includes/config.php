690f496125c944cb68ef7dd3ce09dd56b6db5d7b
ML Media <macowner@Macs-Mac-mini.local>
2015-04-12 20:53:09 -0400

    - testing connection with repo

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,6 @@
 <?php
 
 // define config constants for site-wide includes with different directory structure
-define("BASEURL", "http://localhost/__DEV/quid");
-define("BASEPATH", "C:/xampp/htdocs/__DEV/quid");
+define("BASEURL", "http://localhost/quidkit");
+define("BASEPATH", "C:/xampp/htdocs/quidkit");
 ?>
