8fa65a328c10ef715bc2338b94a832205984daa7
mlmedia <mike@dockstreetmedia.com>
2015-05-21 22:28:02 -0400

    - merging with Mac - ignoring config file

--- includes/config.php
+++ includes/config.php
@@ -2,5 +2,5 @@
 
 // define config constants for site-wide includes with different directory structure
 define("BASEURL", "http://dev.public/quidkit");
-define("BASEPATH", "/Users/macowner/__APPS/__WEB/__PUBLIC/quidkit");
+define("BASEPATH", "C://Users/Owner-1/__APPS/__WEB/__PUBLIC/quidkit");
 ?>
