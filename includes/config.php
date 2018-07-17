5a24f1c8b1bc2c51cfcccc39f79fae1785a8f69d
mlmedia <mike@dockstreetmedia.com>
2018-07-17 03:14:20 -0400

    - test for push

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,6 @@
 <?php
 
 // define config constants for site-wide includes with different directory structure
-define("BASEURL", "http://dev.public/quidkit");
+define("BASEURL", "http://local.public/quidkit");
 define("BASEPATH", "C://Users/Owner-1/__APPS/__WEB/__PUBLIC/quidkit");
 ?>
